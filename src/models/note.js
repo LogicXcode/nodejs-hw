420f387cf21542fb0d3210c1d8c79cbd4e23de25
Logic <saintrpua@gmail.com>
2026-04-22 21:53:51 +0300

    feat: setup mongodb connection and notes crud

---
+++ src/models/note.js
@@ -0,0 +1,36 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const Note = model('Note', noteSchema);
