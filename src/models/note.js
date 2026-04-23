8da485f22fa74f051440b206d407d8ffc0c6f966
Logic <saintrpua@gmail.com>
2026-04-24 02:54:15 +0300

    feat: add auth with sessions and cookies

---
+++ src/models/note.js
@@ -0,0 +1,43 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: 'Todo',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+noteSchema.index(
+  {
+    title: 'text',
+    content: 'text',
+  },
+  {
+    name: 'NoteTextIndex',
+    weights: { title: 10, content: 5 },
+    default_language: 'english',
+  },
+);
+export const Note = model('Note', noteSchema);
---
+++ src/server.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import helmet from 'helmet';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(logger);
+
+// Routes
+app.use(authRoutes);
+app.use(notesRoutes);
+// Handlers
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
+
+// Start server
+const startServer = async () => {
+  try {
+    await connectMongoDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Server failed to start:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
