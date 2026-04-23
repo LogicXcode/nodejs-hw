8638b7cb3c9b1564048e28b1a3137143f655c577
Logic <saintrpua@gmail.com>
2026-04-23 10:34:39 +0300

    feat: add notes validation and mongodb integration

---
+++ src/models/note.js
@@ -0,0 +1,27 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+noteSchema.index({ title: 'text', content: 'text' });
+export const Note = model('Note', noteSchema);
