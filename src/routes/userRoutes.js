2b086ce06f084ef25f6a52a49881c49167be7d2c
Codepint <test@example.com>
2026-09-19 18:12:23 +0300

    Fix missing files, dependencies, and code implementation

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { upload } from '../middleware/multer.js';
+
+const router = Router();
+
+export default router;
